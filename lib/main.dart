c67578c8b5428dcc74a6c257531a7c53c7a1218b
Tomohiro629 <radcell0101@gmail.com>
2022-08-10 13:04:54 +0900

    firebaseインポート

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ramen_map_app/home_page/home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp();
   runApp(const ProviderScope(child: MyApp()));
 }
 
