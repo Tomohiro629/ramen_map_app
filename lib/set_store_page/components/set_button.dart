37b2d8fc39c57e21204735bb0dc3a686485f00fc
Tomohiro629 <radcell0101@gmail.com>
2022-08-11 20:34:53 +0900

    setStoreの見た目と記述変更

--- lib/set_store_page/components/set_button.dart
+++ lib/set_store_page/components/set_button.dart
@@ -1,67 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ramen_map_app/set_store_page/set_store_controller.dart';
 
 class SetButton extends ConsumerWidget {
-  const SetButton({Key? key}) : super(key: key);
+  const SetButton({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final storeSetController = ref.watch(setStoreControllerProvider);
-    return MaterialButton(
-        onPressed: () {
-          try {
-            storeSetController.setStore(
-                name: "name",
-                price: "price",
-                memo: " memo",
-                area: '',
-                latitude: 3.1111,
-                longitude: 4.1234);
-          } catch (e) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text("登録エラー"),
-                backgroundColor: Colors.red,
-              ),
-            );
-          }
-        },
-        child: InkWell(
-          child: Container(
-            width: 200.0,
-            decoration: BoxDecoration(boxShadow: const [
-              BoxShadow(
-                  offset: Offset(0.0, 20.0),
-                  blurRadius: 30.0,
-                  color: Colors.black12)
-            ], color: Colors.white, borderRadius: BorderRadius.circular(22.0)),
-            child: Row(children: <Widget>[
-              Container(
-                height: 50.0,
-                width: 150.0,
-                padding: const EdgeInsets.symmetric(
-                    vertical: 15.0, horizontal: 40.0),
-                decoration: const BoxDecoration(
-                    color: Colors.redAccent,
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(95.0),
-                        topLeft: Radius.circular(95.0),
-                        bottomRight: Radius.circular(200.0))),
-                child: Text(
-                  '登録',
-                  style: Theme.of(context).textTheme.button,
-                ),
-              ),
-              const SizedBox(
-                width: 40.0,
-                child: Icon(
-                  Icons.add_outlined,
-                  color: Colors.redAccent,
-                ),
-              )
-            ]),
+    return InkWell(
+      child: Container(
+        width: 160.0,
+        decoration: BoxDecoration(boxShadow: const [
+          BoxShadow(
+              offset: Offset(0.0, 20.0),
+              blurRadius: 30.0,
+              color: Colors.black12)
+        ], color: Colors.white, borderRadius: BorderRadius.circular(22.0)),
+        child: Row(children: <Widget>[
+          Container(
+            height: 50.0,
+            width: 120.0,
+            padding:
+                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 40.0),
+            decoration: const BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(95.0),
+                    topLeft: Radius.circular(95.0),
+                    bottomRight: Radius.circular(200.0))),
+            child: Text(
+              '登録',
+              style: Theme.of(context).textTheme.button,
+            ),
           ),
-        ));
+          const SizedBox(
+            width: 30.0,
+            child: Icon(
+              Icons.add_outlined,
+              color: Colors.redAccent,
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
--- lib/set_store_page/set_store_page.dart
+++ lib/set_store_page/set_store_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ramen_map_app/set_store_page/components/area_field.dart';
-import 'package:ramen_map_app/set_store_page/components/memo_field.dart';
-import 'package:ramen_map_app/set_store_page/components/price_field.dart';
 import 'package:ramen_map_app/set_store_page/components/set_button.dart';
-import 'package:ramen_map_app/set_store_page/components/store_field.dart';
+import 'package:ramen_map_app/set_store_page/set_store_controller.dart';
 
 class SetStorePage extends ConsumerWidget {
   const SetStorePage({
@@ -13,6 +10,12 @@ class SetStorePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final storeSetController = ref.watch(setStoreControllerProvider);
+    final storeName = TextEditingController();
+    final price = TextEditingController();
+    final memo = TextEditingController();
+    String area = "";
+
     return Scaffold(
         appBar: AppBar(
             title: const Text(
@@ -24,11 +27,172 @@ class SetStorePage extends ConsumerWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              StoreField(),
-              PriceField(),
-              MemoField(),
-              AreaField(),
-              const SetButton()
+              const CircleAvatar(
+                radius: 80,
+                // foregroundImage: imagePickerService.imagePath != null
+                //     ? FileImage(
+                //         imagePickerService.imagePath!,
+                //       )
+                //     : null,
+                backgroundColor: Color.fromARGB(188, 242, 225, 199),
+                child: Text("画像を追加"),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  MaterialButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                        side: const BorderSide(color: Colors.orange)),
+                    onPressed: () {
+                      // imagePickerService.takeCamera();
+                    },
+                    child: const Icon(
+                      Icons.camera_alt,
+                      color: Colors.orange,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 40,
+                  ),
+                  MaterialButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                        side: const BorderSide(color: Colors.orange)),
+                    onPressed: () {
+                      // imagePickerService.takeGallery();
+                    },
+                    child: const Icon(
+                      Icons.photo,
+                      color: Colors.orange,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 350.0,
+                child: TextFormField(
+                  controller: storeName,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.ramen_dining_outlined,
+                        color: Colors.orange,
+                      ),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                        color: Colors.orange,
+                        width: 2,
+                      )),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                        color: Colors.deepOrange,
+                        width: 2,
+                      )),
+                      labelText: "店舗名"),
+                ),
+              ),
+              SizedBox(
+                width: 350.0,
+                child: TextFormField(
+                  controller: price,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.money_outlined,
+                        color: Colors.orange,
+                      ),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                        color: Colors.orange,
+                        width: 2,
+                      )),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                        color: Colors.deepOrange,
+                        width: 2,
+                      )),
+                      labelText: "価格"),
+                ),
+              ),
+              SizedBox(
+                width: 350.0,
+                child: TextFormField(
+                  controller: memo,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.note_outlined,
+                        color: Colors.orange,
+                      ),
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                        color: Colors.orange,
+                        width: 2,
+                      )),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                        color: Colors.deepOrange,
+                        width: 2,
+                      )),
+                      labelText: "メモ"),
+                ),
+              ),
+              SizedBox(
+                width: 350.0,
+                child: DropdownButtonFormField(
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Colors.orange,
+                          width: 2,
+                        )),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Colors.deepOrange,
+                          width: 2,
+                        )),
+                        hintText: "エリア選択",
+                        prefixIcon: Icon(
+                          Icons.map_outlined,
+                          color: Colors.orangeAccent,
+                        )),
+                    items: const [
+                      DropdownMenuItem(value: "北区", child: Text("北区")),
+                      DropdownMenuItem(value: "左京区", child: Text("左京区")),
+                      DropdownMenuItem(value: "右京区", child: Text("右京区")),
+                      DropdownMenuItem(value: "上京区", child: Text("上京区")),
+                      DropdownMenuItem(value: "中京区", child: Text("中京区")),
+                      DropdownMenuItem(value: "下京区", child: Text("下京区")),
+                      DropdownMenuItem(value: "南区", child: Text("南区")),
+                      DropdownMenuItem(value: "西京区", child: Text("西京区")),
+                      DropdownMenuItem(value: "東山区", child: Text("東山区")),
+                      DropdownMenuItem(value: "山科区", child: Text("山科区")),
+                      DropdownMenuItem(value: "伏見区", child: Text("伏見区")),
+                      DropdownMenuItem(value: "京都市外", child: Text("京都市外")),
+                    ],
+                    onChanged: (String? value) {
+                      area = value!;
+                    }),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  try {
+                    storeSetController.setStore(
+                        name: storeName.text,
+                        price: price.text,
+                        memo: memo.text,
+                        area: area,
+                        latitude: 10,
+                        longitude: 20);
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("登録エラー"),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  }
+                },
+                child: const SetButton(),
+              ),
             ],
           ),
         ));
