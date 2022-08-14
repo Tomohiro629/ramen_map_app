a546221b23bb2ff36c38e087fbc4afbf3e84d722
Tomohiro629 <radcell0101@gmail.com>
2022-08-14 17:40:08 +0900

    ログイン・サインアップボタンのwidget分けた

---
+++ lib/home_page/components/login_button.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:ramen_map_app/login_page/login_page.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      child: InkWell(
+        child: Container(
+          width: 200.0,
+          decoration: BoxDecoration(boxShadow: const [
+            BoxShadow(
+                offset: Offset(0.0, 20.0),
+                blurRadius: 30.0,
+                color: Colors.black12)
+          ], color: Colors.white, borderRadius: BorderRadius.circular(22.0)),
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 50.0,
+                width: 150.0,
+                padding: const EdgeInsets.symmetric(
+                    vertical: 15.0, horizontal: 40.0),
+                decoration: const BoxDecoration(
+                    color: Colors.orangeAccent,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(95.0),
+                        topLeft: Radius.circular(95.0),
+                        bottomRight: Radius.circular(200.0))),
+                child: Text(
+                  'ログイン',
+                  style: Theme.of(context).textTheme.button,
+                ),
+              ),
+              const SizedBox(
+                width: 40.0,
+                child: Icon(
+                  Icons.login_outlined,
+                  color: Colors.orange,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const LoginPage()));
+      },
+    );
+  }
+}
---
+++ lib/home_page/components/signup_button copy.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:ramen_map_app/signup_page/signup_page.dart';
+
+class SignupButton extends StatelessWidget {
+  const SignupButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      child: InkWell(
+        child: Container(
+          width: 200.0,
+          decoration: BoxDecoration(boxShadow: const [
+            BoxShadow(
+                offset: Offset(0.0, 20.0),
+                blurRadius: 50.0,
+                color: Colors.black12)
+          ], color: Colors.white, borderRadius: BorderRadius.circular(22.0)),
+          child: Row(children: <Widget>[
+            Container(
+              height: 50.0,
+              width: 150.0,
+              padding:
+                  const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+              decoration: const BoxDecoration(
+                  color: Colors.greenAccent,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(95.0),
+                      topLeft: Radius.circular(95.0),
+                      bottomRight: Radius.circular(200.0))),
+              child: Text(
+                'アカウント作成',
+                style: Theme.of(context).textTheme.button,
+              ),
+            ),
+            const SizedBox(
+              width: 40.0,
+              child: Icon(Icons.edit_outlined, color: Colors.green),
+            )
+          ]),
+        ),
+      ),
+      onPressed: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const SignupPage()));
+      },
+    );
+  }
+}
