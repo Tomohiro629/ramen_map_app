781e913135255242f4e99e25cd0534d5f798d6c1
Tomohiro629 <radcell0101@gmail.com>
2022-08-17 12:23:18 +0900

    ラーメンリストUI変更

--- lib/store_list_page/components/store_data.dart
+++ lib/store_list_page/components/store_data.dart
@@ -34,152 +34,134 @@ class StoreData extends ConsumerWidget {
                 ),
               ],
             ),
-            SizedBox(
-              height: 340.0.h,
-              child: Stack(
-                children: [
-                  Card(
-                    color: Colors.amber[100],
-                    clipBehavior: Clip.antiAlias,
-                    elevation: 14.0,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0)),
-                    child: Stack(
-                      children: [
-                        Center(
-                          child: Column(
-                            children: <Widget>[
-                              Stack(
-                                children: <Widget>[
-                                  Ink(
-                                    height: 280.0.h,
+            Stack(
+              children: [
+                Card(
+                  clipBehavior: Clip.antiAlias,
+                  elevation: 14.0,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 230.0.h,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5.0),
+                            image: DecorationImage(
+                              image: NetworkImage(store.ramenImage),
+                              fit: BoxFit.cover,
+                            )),
+                        child: InkWell(
+                          splashColor: const Color.fromARGB(119, 255, 184, 77),
+                          onTap: () {
+                            mapController.addMarker(store);
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => MapPage(
+                                          storeId: store.storeId,
+                                        )));
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 230.0.h,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Align(
+                              alignment: Alignment.bottomLeft,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Text(
+                                        store.name,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 20.0.sp),
+                                      ),
+                                      Text(
+                                        "味分類： ${store.taste}",
+                                        style: TextStyle(
+                                            color: Colors.orange,
+                                            fontSize: 15.0.sp),
+                                      ),
+                                    ],
+                                  ),
+                                  Container(
                                     decoration: BoxDecoration(
                                         borderRadius:
-                                            BorderRadius.circular(5.0),
-                                        image: DecorationImage(
-                                          image: NetworkImage(store.ramenImage),
-                                          fit: BoxFit.cover,
-                                        )),
-                                    child: InkWell(
-                                      splashColor: const Color.fromARGB(
-                                          119, 255, 184, 77),
-                                      onTap: () {
-                                        mapController.addMarker(store);
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) => MapPage(
-                                                      storeId: store.storeId,
-                                                    )));
-                                      },
+                                            BorderRadius.circular(10.0),
+                                        color: Colors.black87),
+                                    child: Text(
+                                      " 価格：${store.price}円",
+                                      style: TextStyle(
+                                        fontSize: 18.0.sp,
+                                        color: Colors.white,
+                                      ),
                                     ),
                                   ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 210.0, left: 5.0),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          store.name,
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 20.0.sp),
-                                        ),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Text(
-                                              "味分類： ${store.taste}",
-                                              style: TextStyle(
-                                                  color: Colors.orange,
-                                                  fontSize: 15.0.sp),
-                                            ),
-                                            Container(
-                                              decoration: BoxDecoration(
-                                                  borderRadius:
-                                                      BorderRadius.circular(
-                                                          10.0),
-                                                  color: Colors.black87),
-                                              child: Text(
-                                                " 価格：${store.price}円",
-                                                style: TextStyle(
-                                                  fontSize: 18.0.sp,
-                                                  color: Colors.white,
-                                                ),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  )
                                 ],
                               ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 5.0),
-                                child: Text(
-                                  "一言メモ:${store.memo}",
-                                  maxLines: 2,
-                                ),
-                              )
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50.0),
+                color: Colors.amber[100],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      "一言メモ:${store.memo}",
+                      maxLines: 2,
+                      style: TextStyle(fontSize: 15.0.sp),
                     ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                          color: Colors.blueAccent,
+                          icon: const Icon(Icons.edit_outlined),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        EditStorePage(store: store)));
+                          }),
+                      IconButton(
+                          color: Colors.redAccent,
+                          icon: const Icon(Icons.delete_outline),
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (childContext) {
+                                  return DeleteCheckDialog(store: store);
+                                });
+                          }),
+                    ],
                   ),
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SizedBox(
-                  width: 100.0.w,
-                  child: MaterialButton(
-                      color: Colors.blueAccent,
-                      child: Row(
-                        children: const [
-                          Icon(Icons.edit_note_outlined),
-                          Text(
-                            "編集",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    EditStorePage(store: store)));
-                      }),
-                ),
-                SizedBox(
-                  width: 100.0.w,
-                  child: MaterialButton(
-                      color: Colors.redAccent,
-                      child: Row(
-                        children: const [
-                          Icon(Icons.delete_outline),
-                          Text(
-                            "削除",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                      onPressed: () {
-                        showDialog(
-                            context: context,
-                            builder: (childContext) {
-                              return DeleteCheckDialog(store: store);
-                            });
-                      }),
-                ),
-              ],
             ),
           ],
         ),
