ad4fd92a9f3615d807612401f5db8a15480455d7
Tomohiro629 <radcell0101@gmail.com>
2022-08-24 15:44:14 +0900

    image_cropperの追加

---
+++ lib/service/image_cropper_service.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_cropper/image_cropper.dart';
+
+final imageCropperServiceProvider =
+    ChangeNotifierProvider<ImageCropperService>((ref) {
+  return ImageCropperService();
+});
+
+class ImageCropperService extends ChangeNotifier {
+  Future<File?> cropImage({required File imageFile}) async {
+    CroppedFile? croppedFile = await ImageCropper().cropImage(
+      sourcePath: imageFile.path,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        CropAspectRatioPreset.ratio3x2,
+        CropAspectRatioPreset.original,
+        CropAspectRatioPreset.ratio4x3,
+        CropAspectRatioPreset.ratio16x9
+      ],
+      uiSettings: [
+        AndroidUiSettings(
+            toolbarTitle: '画像編集',
+            toolbarColor: Colors.deepOrange,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+      ],
+    );
+    if (croppedFile == null) return null;
+    return File(croppedFile.path);
+  }
+}
--- lib/service/image_picker_service.dart
+++ lib/service/image_picker_service.dart
@@ -3,13 +3,16 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:ramen_map_app/service/image_cropper_service.dart';
 
 final imagePickerServiceProvider =
     ChangeNotifierProvider<ImagePickerService>((ref) {
-  return ImagePickerService();
+  return ImagePickerService(ref.read);
 });
 
 class ImagePickerService extends ChangeNotifier {
+  final Reader reader;
+  ImagePickerService(this.reader);
   final _picker = ImagePicker();
   File? imagePath;
 
@@ -17,7 +20,9 @@ class ImagePickerService extends ChangeNotifier {
     final picekdfile =
         await _picker.pickImage(source: ImageSource.camera, imageQuality: 80);
     if (picekdfile != null) {
-      imagePath = File(picekdfile.path);
+      File? img = File(picekdfile.path);
+      img = await reader(imageCropperServiceProvider).cropImage(imageFile: img);
+      imagePath = img;
     }
     notifyListeners();
   }
@@ -26,7 +31,9 @@ class ImagePickerService extends ChangeNotifier {
     final picekdfile =
         await _picker.pickImage(source: ImageSource.gallery, imageQuality: 80);
     if (picekdfile != null) {
-      imagePath = File(picekdfile.path);
+      File? img = File(picekdfile.path);
+      img = await reader(imageCropperServiceProvider).cropImage(imageFile: img);
+      imagePath = img;
     }
     notifyListeners();
   }
