da0d7bf6ab241e9377e031cd11cd64dd2dde121f
Tomohiro629 <radcell0101@gmail.com>
2022-08-29 14:51:31 +0900

    お店リストの画像読み込みの追加

--- lib/store_list_page/components/store_image.dart
+++ lib/store_list_page/components/store_image.dart
@@ -18,139 +18,150 @@ class StoreImage extends ConsumerWidget {
         padding: const EdgeInsets.all(10.0),
         child: Stack(
           children: [
-            Card(
-              clipBehavior: Clip.antiAlias,
-              elevation: 14.0,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0)),
-              child: Stack(
-                children: [
-                  Container(
-                    height: 230.0.h,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5.0),
-                        image: DecorationImage(
-                          image: NetworkImage(store.ramenImage),
-                          fit: BoxFit.cover,
-                        )),
-                    child: InkWell(
-                      splashColor: const Color.fromARGB(119, 255, 184, 77),
-                      onTap: () {
-                        showModalBottomSheet(
-                            isScrollControlled: true,
-                            shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.vertical(
-                                  top: Radius.circular(30)),
-                            ),
-                            context: context,
-                            builder: (BuildContext context) {
-                              return StoreDataBottomSheet(store: store);
-                            });
-                        storeRepository.setStore(store: store.deleteCheck());
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    height: 230.0.h,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Container(
-                            child: !store.isAddCheck
-                                ? Text(
-                                    "new",
-                                    style: TextStyle(
-                                        color: const Color.fromARGB(
-                                            255, 251, 1, 159),
-                                        fontSize: 20.0.sp),
-                                  )
-                                : null,
-                          ),
-                        ),
-                        const Align(
-                          alignment: Alignment.bottomLeft,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  color: const Color.fromARGB(173, 48, 46, 46),
-                                ),
-                                child: Text(
-                                  store.name,
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20.0.sp),
-                                ),
+            SizedBox(
+              height: 250.0.h,
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                elevation: 14.0,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0)),
+                child: Stack(
+                  children: [
+                    const Center(
+                        child: CircularProgressIndicator(
+                      color: Colors.deepOrange,
+                    )),
+                    Container(
+                      height: 250.0.h,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          image: DecorationImage(
+                            image: NetworkImage(store.ramenImage),
+                            fit: BoxFit.cover,
+                          )),
+                      child: InkWell(
+                        splashColor: const Color.fromARGB(119, 255, 184, 77),
+                        onTap: () {
+                          showModalBottomSheet(
+                              isScrollControlled: true,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.vertical(
+                                    top: Radius.circular(30)),
                               ),
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  color: const Color.fromARGB(224, 48, 46, 46),
-                                ),
-                                child: IconButton(
-                                  onPressed: () {
-                                    !store.isFavorite
-                                        ? storeRepository.setStore(
-                                            store: store.addFavorite())
-                                        : storeRepository.setStore(
-                                            store: store.deleteFavorite());
-                                    ScaffoldMessenger.of(context)
-                                        .showSnackBar(!store.isFavorite
-                                            ? SnackBar(
-                                                content: Text(
-                                                  "${store.name}を\nお気に入りに登録しました！",
-                                                  textAlign: TextAlign.center,
-                                                  style: const TextStyle(
-                                                      color: Colors.white),
-                                                ),
-                                                backgroundColor:
-                                                    const Color.fromARGB(
-                                                        219, 209, 67, 186),
-                                                duration:
-                                                    const Duration(seconds: 1),
-                                              )
-                                            : SnackBar(
-                                                content: Text(
-                                                  "${store.name}を\nお気に入りから削除しました。",
-                                                  textAlign: TextAlign.center,
-                                                  style: const TextStyle(
-                                                      color: Colors.white),
-                                                ),
-                                                backgroundColor:
-                                                    const Color.fromARGB(
-                                                        219, 67, 209, 140),
-                                                duration:
-                                                    const Duration(seconds: 1),
-                                              ));
-                                  },
-                                  icon: !store.isFavorite
-                                      ? const Icon(
-                                          Icons.favorite_outline,
-                                          size: 20.0,
-                                        )
-                                      : const Icon(
-                                          Icons.favorite_outline_outlined,
-                                          color:
-                                              Color.fromARGB(179, 246, 14, 173),
-                                          size: 20.0,
-                                        ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                              context: context,
+                              builder: (BuildContext context) {
+                                return StoreDataBottomSheet(store: store);
+                              });
+                          storeRepository.setStore(store: store.deleteCheck());
+                        },
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
+            ),
+            Stack(
+              children: [
+                SizedBox(
+                  height: 250.0.h,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          child: !store.isAddCheck
+                              ? Text(
+                                  "new",
+                                  style: TextStyle(
+                                      color: const Color.fromARGB(
+                                          255, 251, 1, 159),
+                                      fontSize: 20.0.sp),
+                                )
+                              : null,
+                        ),
+                      ),
+                      const Align(
+                        alignment: Alignment.bottomLeft,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                color: const Color.fromARGB(173, 48, 46, 46),
+                              ),
+                              child: Text(
+                                store.name,
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20.0.sp),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                color: const Color.fromARGB(224, 48, 46, 46),
+                              ),
+                              child: IconButton(
+                                onPressed: () {
+                                  !store.isFavorite
+                                      ? storeRepository.setStore(
+                                          store: store.addFavorite())
+                                      : storeRepository.setStore(
+                                          store: store.deleteFavorite());
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(!store.isFavorite
+                                          ? SnackBar(
+                                              content: Text(
+                                                "${store.name}を\nお気に入りに登録しました！",
+                                                textAlign: TextAlign.center,
+                                                style: const TextStyle(
+                                                    color: Colors.white),
+                                              ),
+                                              backgroundColor:
+                                                  const Color.fromARGB(
+                                                      219, 209, 67, 186),
+                                              duration:
+                                                  const Duration(seconds: 1),
+                                            )
+                                          : SnackBar(
+                                              content: Text(
+                                                "${store.name}を\nお気に入りから削除しました。",
+                                                textAlign: TextAlign.center,
+                                                style: const TextStyle(
+                                                    color: Colors.white),
+                                              ),
+                                              backgroundColor:
+                                                  const Color.fromARGB(
+                                                      219, 67, 209, 140),
+                                              duration:
+                                                  const Duration(seconds: 1),
+                                            ));
+                                },
+                                icon: !store.isFavorite
+                                    ? const Icon(
+                                        Icons.favorite_outline,
+                                        size: 20.0,
+                                      )
+                                    : const Icon(
+                                        Icons.favorite_outline_outlined,
+                                        color:
+                                            Color.fromARGB(179, 246, 14, 173),
+                                        size: 20.0,
+                                      ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
